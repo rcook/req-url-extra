f89d5b0243303ea173f3d5632177edecdb765f6b
Richard Cook <rcook@rcook.org>
2019-02-13 08:03:44 -0800

    Depend on req >= 2.0.0 && < 2.1.0 and use defaultHttpConfig instead of def

--- sample/Main.hs
+++ sample/Main.hs
@@ -3,13 +3,20 @@
 module Main (main) where
 
 import           Data.Aeson (Value)
-import           Data.Default.Class (def)
-import           Network.HTTP.Req (GET(..), NoReqBody(..), jsonResponse, req, responseBody, runReq)
+import           Network.HTTP.Req
+                    ( GET(..)
+                    , NoReqBody(..)
+                    , defaultHttpConfig
+                    , jsonResponse
+                    , req
+                    , responseBody
+                    , runReq
+                    )
 import           Network.HTTP.Req.Url.Extra (toUrlHttps)
 import           Text.URI.QQ (uri)
 
 main :: IO ()
 main = do
     let Just (url, opts) = toUrlHttps [uri|https://httpbin.org/get?a=value0&a=value1&b=value3#fragment|]
-    v <- runReq def $ req GET url NoReqBody jsonResponse opts
+    v <- runReq defaultHttpConfig $ req GET url NoReqBody jsonResponse opts
     print (responseBody v :: Value)
